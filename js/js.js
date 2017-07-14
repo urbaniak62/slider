d8358aa75cca5a660fad49d191ad88db0abdba30
urbaniak <urbaniaknicolas62@gmail.com>
2017-07-14 14:52:30 +0200

    changement-de-photos

--- js/js.js
+++ js/js.js
@@ -7,11 +7,18 @@ var arriere=document.getElementById=("arriere");
 
 /*----------------chemin image dans le tableau------*/
 
-image.push("img/images2.jpg");
-image.push("img/images4.jpg");
-image.push("img/images.jpeg");
-image.push("img/images1.jpeg");
-image.push("img/images3.jpeg");
+image.push("img/3d.jpg");
+image.push("img/3d1.jpg");
+image.push("img/3d2.jpg");
+image.push("img/3d3.jpg");
+image.push("img/3d4.jpg");
+image.push("img/3d5.jpg");
+image.push("img/3d6.jpg");
+image.push("img/3d7.jpg");
+image.push("img/3d8.jpg");
+image.push("img/3d9.jpg");
+image.push("img/3d10.jpg");
+
 // console.log(image);
 
 
@@ -19,12 +26,13 @@ function img(){
 
   document.getElementsByTagName("img")[0].src = image[i];
   // console.log(document.getElementsByTagName("img")[0].src);
-  if(i < image.length ){
+  if(i < image.length -1 ){
     i++;
   }else {
     i = 0;
 
   }
+
   /*------------declaration du tps sur le tagName img--*/
   setTimeout(img, time);
 }
